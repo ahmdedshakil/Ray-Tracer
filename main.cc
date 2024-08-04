54a59ad90caa4315f9de5f30665a644a5698274f
Ahmed Shakil <ashakil@berkeley.edu>
2024-08-03 22:34:41 -0700

    Made some more progress, started working with rays.

--- main.cc
+++ main.cc
@@ -1,23 +1,57 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
 using namespace std;
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
     // Image details
 
-    int width = 256;
-    int height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Viewport vectors
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Delta vectors
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // 0,0 pixel, a.k.a top left 
+    auto viewport_upper_left = camera_center
+                             - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Now render
 
-    cout << "P3\n" << width << ' ' << height << "\n255\n";
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int i = 0; i < height; i++) {
-        clog << "\rLines of output remaining: " << (height - i) << ' ' << flush;
-        for (int j = 0; j < width; j++) {
-            auto pixel_color = color(double(j)/(width-1), double(i)/(height-1), 0);
+    for (int i = 0; i < image_height; i++) {
+        clog << "\rLines of output remaining: " << (image_height - i) << ' ' << flush;
+        for (int j = 0; j < image_width; j++) {
+            auto pixel_center = pixel00_loc + (j * pixel_delta_u) + (i * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(cout, pixel_color);
         }
     }
